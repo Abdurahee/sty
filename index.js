ebabadd42be6603a9a25f9c5b573415f35fea285
I am Tope Yusuf <125209599+Abdurahee@users.noreply.github.com>
2023-12-07 13:22:11 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -2,26 +2,26 @@
 
 
 // welcome greeting
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('button');
+// let myButton = document.querySelector('button');
+// let myHeading = document.querySelector('button');
 
-function setUserName() {
-  let myName = prompt('Please enter your name.');
-  if(!myName) {
-    setUserName();
-  } else {
-    localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'welcome to my webpage, ' + myName;
-  }
-}
+// function setUserName() {
+//   let myName = prompt('Please enter your name.');
+//   if(!myName) {
+//     setUserName();
+//   } else {
+//     localStorage.setItem('name', myName);
+//     myHeading.innerHTML = 'welcome to my webpage, ' + myName;
+//   }
+// }
 
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  let storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'welcome to my webpage, ' + storedName;
-}
+// if(!localStorage.getItem('name')) {
+//   setUserName();
+// } else {
+//   let storedName = localStorage.getItem('name');
+//   myHeading.innerHTML = 'welcome to my webpage, ' + storedName;
+// }
 
-myButton.onclick = function() {
-  setUserName();
-}
+// myButton.onclick = function() {
+//   setUserName();
+// }
